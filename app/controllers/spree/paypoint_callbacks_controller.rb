e52e48259a4f6204c57e9519f1f11be7986d041c
Gary Taylor <garywork@laptop.lan>
2011-01-20 06:37:44 +0000

    Home page almost done

--- app/controllers/spree/paypoint_callbacks_controller.rb
+++ app/controllers/spree/paypoint_callbacks_controller.rb
@@ -4,7 +4,7 @@ module Spree
     def notify
       payment_method=PaymentMethod.find_by_id(params[:payment_method_id])
 
-      n=ActiveMerchant::Billing::Integrations::Paypoint.notification(request.query_string,:digest=>request.url.gsub(/hash=\w*/,'').gsub(/^https*:\/\/.*?/,'/') + payment_method.preferred_password)
+      n=ActiveMerchant::Billing::Integrations::Paypoint.notification(request.query_string,:digest=>request.fullpath.gsub(/hash=\w*/,'') + payment_method.preferred_password)
       order=Order.find_by_id(n.item_id)
       Rails.logger.warn "Warning:: Attempt at spoofind the paypoint callback from #{request.ip}" unless n.from_paypoint?
       if n.from_paypoint? and n.completed? and n.amount==order.amount
